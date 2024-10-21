d5ee01d96386d3dddda28800031b5badef3b8816
grand <grand@JESS>
2024-10-21 11:55:34 -0500

    Agregar archivos de proyecto.

---
+++ ErickGranda-TallerFut/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ErickGranda_TallerFut.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
